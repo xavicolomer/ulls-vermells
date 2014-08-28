e0af3d9d269c66555d46905165bd2158c4ff9c70
Xavi Colomer <xavicolomer@me.com>
2014-08-28 18:10:33 +0200

    Afegit informació sobre els trams i algunes millores

--- engine.js
+++ engine.js
@@ -1,22 +1,60 @@
-values = ['#ff0000', '#FF5500', '#FFaa00', '#DDDD13','#91C034', '#40AC17'];
+colors = ['#40AC17','#91C034', '#DDDD13', '#FFaa00', '#FF5500', '#ff0000'];
+stages = ['1750','3500', '5250', '7000', '8750', '10500'];
+titles = ['Baixa','Poca', 'Mitja', 'Alta', 'Quasi Plena', 'Plena'];
 
 var count = 0;
 jQuery('#chart .nv-series-0').children('g').each(function(){ 
-
-    jQuery(this).attr('style','fill:' + values[count] + ';');
+    jQuery(this).attr('style','fill:' + colors[count] + ';');
     count = count + 1;
 })
 
 var count = 0;
 jQuery('#chartcomarca2').find('.nv-group').each(function(){ 
-    jQuery(this).attr('style','fill:' + values[count] + ';');
+    jQuery(this).attr('style','fill:' + colors[count] + ';');
     count = count + 1;
 })
 
 var count = 0;
 jQuery('#chartcomarca2').find('.nv-legend-symbol').each(function(){ 
-    jQuery(this).attr('style','fill:' + values[count] + ';');
+    jQuery(this).attr('style','fill:' + colors[count] + ';');
     count = count + 1;
 })
 
+var temp = [];
+jQuery('#chart .nv-series-0').children('g').each(function(){ 
+    title = jQuery(this).children('text').html();
+    num = title.split(' (')[0];
+    temp.push(num);
+})
 
+jQuery('#chart').css('width', 470);
+jQuery('#chart').css('float', 'left');
+
+var min = 0;
+var max = 0;
+var html = '<table id="summary">';
+html += '<tr class="uv-header"><td class="tram">Quantitat de Gent</td><td class="number">Pessimista</td><td class="number">Mitja</td><td class="number">Optimista</td></tr>';
+
+for (var i = 0; i < stages.length; ++i )
+{
+    var initial = 0;
+    if (i != 0)
+        initial += temp[i]*stages[i-1];
+    
+    min += initial;    
+    max += temp[i]*stages[i];
+
+    klass = '';
+    if (i % 2 == 0)
+        klass=" class='uv-odd'";
+
+    html += '<tr '+klass+'><td class="tram">'+titles[i]+' ('+ (i == 0 ? 0 : stages[i-1])+'-'+stages[i]+')</td><td class="number">'+initial+'</td><td class="number">'+((initial+temp[i]*stages[i])*0.5)+'</td><td class="number">'+temp[i]*stages[i]+'</td></tr>';
+}
+
+html += '<tr class="uv-bottom"><td  class="tram">Total</td><td class="number">'+min+'</td><td class="number">'+((min+max)*0.5)+'</td><td class="number">'+max+'</td></tr>';
+
+html += '</table>';
+
+console.log(min, max);
+
+jQuery("#chart").after(html);
